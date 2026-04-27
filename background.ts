aee29e23c3ddee065be68955a65a54fb2f5ebbb3
Adem Kouki <ademkingnew@gmail.com>
2026-04-27 18:19:51 +0100

    update permissions

--- background.ts
+++ background.ts
@@ -19,27 +19,17 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 })
 
 chrome.commands.onCommand.addListener((command, tab) => {
-  if (command === "convert-to-markdown") {
-    if (tab?.id) {
-      chrome.tabs
-        .sendMessage(tab.id, { action: "convert-to-markdown" })
-        .catch((err) => console.log("Content script not ready.", err))
-    } else {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs[0]?.id) {
-          chrome.tabs
-            .sendMessage(tabs[0].id, { action: "convert-to-markdown" })
-            .catch((err) => console.log("Content script not ready.", err))
-        }
-      })
-    }
+  if (command === "convert-to-markdown" && tab?.id) {
+    chrome.tabs
+      .sendMessage(tab.id, { action: "convert-to-markdown" })
+      .catch((err) => console.log("Content script not ready.", err))
   }
 })
 
 // Listen for messages from the content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "open-markdown-tab") {
-    // Open the new tab page provided by Plasmo
+    // Open the new tab page
     chrome.tabs.create({ url: chrome.runtime.getURL("tabs/markdown.html") })
   }
 })
